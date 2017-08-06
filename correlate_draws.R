0fd416236fd755fb1c17d3b16f7783257937cf74
Nafis Sadat <sadatnfs@uw.edu>
2017-08-06 13:20:47 -0700

    Added the stacking and correlating functions

--- correlate_draws.R
+++ correlate_draws.R
@@ -1,12 +1,51 @@
 require(data.table)
+require(MASS)
+require(stringr)
+require(doParallel)
+require(abind)
+require(Matrix)
+
 source("./helper_functions.R")
 
-correlate_draws <- function(draw_data, corr_vector=None,
-                            corr_matrix=None, corr_vars=None) {
+## Note: the datasets MUST be combined and melted first! The combined part more important 
+
+## So far our function is only dealing with a single dataset, and that just might be easier: 
+# to have the user supply a prepped data with very specific dims
+
+#### Expected dataset for user:
+
+## One dataset with the following variables, so that they can be passed on to our function:
+
+## corr_vars : a (vector of) variable(s) which the user wants to induce correlation over
+## other_vars : a (vector of) variable(s) which the user wants to not change correlation over
+## a third variable: the dimension which the user wants to correlate over (usually draws)
+## fourth variable: the actual data
+
+
+correlate_draws <- function(draw_data, draw_names, corr_vector=NA,
+                            corr_matrix=NA, corr_vars=NA, other_vars=NA, return_wide=F) {
+
+	## Run checks
     check_input_format(draw_data, corr_vars, corr_vector, corr_matrix)
+    
+    ## Generating corr matrix
     correlation_matrix <- get_corr_matrix(corr_vector, corr_matrix)
-    stacked_data <- stack_data(draw_data, corr_vars)
-    correlated_stack <- correlate_stack(stacked_data, correlation_matrix)
-    correlated_data <- unstack_data(correlated_stack)
-    return correlated_data
+    
+    ## Stack the variables
+    stacked_data <- stack_data(draw_data, corr_vars, other_vars)
+
+    ## Convert to multidimensional array
+    data_in_array <- to_array(stacked_data, corr_vars, other_vars)
+    colnames(data_in_array) <- colnames(stacked_data)
+
+    ## Correlate the data
+    correlated_stack <- correlate_stack(data_in_array, correlation_matrix)
+    
+    ## Unstack the data
+    correlated_data <- unstack_data(correlated_stack, corr_vars, other_vars, 
+    								draw_name = colnames(data_in_array)[3], 
+    								data_col_name = colnames(data_in_array)[4], 
+    								wide_on_draws= return_wide)   
+
+    return(correlated_data)
 }
--- helper_functions.R
+++ helper_functions.R
@@ -42,14 +42,111 @@ get_corr_matrix <- function(corr_vector, corr_matrix) {
     print("TODO: write this function")
 }
 
-stack_data <- function(data, corr_vars) {
-    print("TODO: write this function")
+stack_data <- function(data, corr_vars, other_vars) {    
+    to_stack_name <- paste(corr_vars, collapse="__")
+    no_stack_name <- paste(other_vars, collapse="__")
+    
+    data[, paste0(corr_vars_name) := do.call(paste, c(data[, .SD, .SDcols = corr_vars], sep = "__")) ]
+    data[, paste0(other_vars_name) := do.call(paste, c(data[, .SD, .SDcols = other_vars], sep = "__"))]
+    
+    ## Remove the non-stacked vars
+    data[, paste0(corr_vars_name) := NULL]
+    data[, paste0(other_vars_name) := NULL]
+
+    ## Make sure we have a 4 column dataset
+    assert_that(ncol(data) == 4, msg = "The dataset has other dimensions not used. Please make sure that there are no extra variables going in.")
+
+    return(data)
 }
 
-correlate_stack <- function(stack, corr_matrix) {
-    print("TODO: write this function")
+to_array <- function(data, corr_vars, other_vars) {
+
+  ## Get a single vector with the names of the vars 
+  var_melters <- c(paste(other_vars, collapse= "__"), paste(corr_vars, collapse= "__"))
+
+  ## Get colnames
+  no_corr <- colnames(data)[1]
+  yes_corr <- colnames(data)[2]
+  corr_dim <- colnames(data)[3]
+  data_col <- colnames(data)[4]
+
+  ## Convert to array
+  return(reshape2::acast( get(no_corr) ~ get(yes_corr) ~ get(corr_dim), value.var = data_col))
+
 }
 
-unstack_data <- function(stack) {
-    print("TODO: write this function")
+correlate_stack <- function(data, corr_matrix) {
+        
+  L <- dim(X)[2]
+  D <- dim(X)[3]
+  Xsum <- apply(X, c(2, 3), sum)
+  mvdat <- mvrnorm(n=D, mu=0 * 1:L, Sigma=corr_matrix, empirical=TRUE)
+  ranks <- apply(mvdat, 2, rank, ties.method="first")
+  sortedXsim <- apply(Xsum, 1, function(x) sort(x, index.return=TRUE)$ix)
+  sortedX <- X
+  for(i in 1:L){
+    sortedX[,i,] <- X[,i,sortedXsim[,i]]
+  }
+  Xcorr <- sortedX
+  for(i in 1:L){
+    Xcorr[,i,] <- sortedX[,i,ranks[,i]]
+  }
+  
+  return(data.table(melt(Xcorr)))
+  
 }
+
+
+
+unstack_data <- function(data, corr_vars, other_vars, draw_name = "draw_num", data_col_name = "data", wide_on_draws = F) {
+    
+    
+    #### Purpose: Split the variables according to the length of the corring_over and not_corring_over vectors
+
+    
+    ## Clean up column names    
+    setDT(data)[, paste0("not_corr", 1:length(other_vars)) := 
+                                       tstrsplit(get(var_melters[1]), "__", type.convert = TRUE, fixed = TRUE)]
+    setDT(data)[, paste0("to_corr", 1:length(corr_vars)) := 
+                                       tstrsplit(get(var_melters[2]), "__", type.convert = TRUE, fixed = TRUE)]
+
+
+    ## Keep what we need:
+    data_processed <- data[, .SD, .SDcols = c(paste0("not_corr", 1:length(other_vars)),
+                                  paste0("to_corr", 1:length(corr_vars)),
+                                  paste0(draw_name), paste0(data) )]
+    
+    ## Rename columns back
+    for(i in 1:length(c(other_vars, corr_vars))) {
+        colnames(data_processed)[i] <- c(other_vars, corr_vars)[i] 
+    }
+
+    ## Cast the draws out wide?
+    if(wide_on_draws) {
+        data_processed <- dcast(data_processed, other_vars + corr_vars ~ draw_name, value.var = paste0(data) )
+    }
+    
+
+    return(data_processed)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
