6aa41e1c50bd5cf47f9a31f6654739addd90d216
Nafis Sadat <sadatnfs@uw.edu>
2017-08-31 21:01:51 -0700

    Updating the master branch again

--- correlating_edu_LDI_TFR/tfr_nc_to_csv.py
+++ correlating_edu_LDI_TFR/tfr_nc_to_csv.py
@@ -10,7 +10,7 @@ for scen in [-1, 0, 1]:
     scenarios = scenarios.loc[{'scenario':scen}]
     scenarios = scenarios.to_dataframe()
     scenarios = scenarios.drop('scenario', axis=1)
-    scenarios = scenarios.unstack()
+    scenarios = scenarios.unstack('draw')
     scenarios = scenarios['tfr'].reset_index()
     scenarios =  scenarios.rename(columns={i:'draw_{}'.format(i) for i in range(1000)})
     scenarios.to_csv('/ihme/forecasting/data/fbd_scenarios_data/forecast/asfr/best/tfr_csv/tfr_draws_scenario{}.csv'.format(scen), index=False)
